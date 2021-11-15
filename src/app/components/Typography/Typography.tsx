8e4919d39cd90f0a3e87e857d6bd2afdf0e8a1a5
Felix Kirchhoff <mailfk@phispace.eu>
2021-11-15 15:47:20 +0100

    Cleanup

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -16,15 +16,12 @@ function getElementByType(type: string) {
   switch (type.toUpperCase()) {
     case 'H1':
       return H1;
-
     case 'H2':
       return H2;
-
     case 'H3':
       return H3;
     case 'H4':
       return H4;
-
     default:
       return H1;
   }
