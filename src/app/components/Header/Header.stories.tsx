b25d269f8c96ef1a6919159a54ba061e5169f0d3
Walied Rashied <w.rashied@icloud.com>
2021-11-15 17:24:44 +0100

    formatting

--- src/app/components/Header/Header.stories.tsx
+++ src/app/components/Header/Header.stories.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import Header from './Header';
 
 export default {
-    title: 'Component/Header',
-    component: Header,
+  title: 'Component/Header',
+  component: Header,
 };
 
-
-
-export const Regular = (): JSX.Element => (
-    <Header>Top Five</Header>
-);
-
+export const Regular = (): JSX.Element => <Header>Top Five</Header>;
