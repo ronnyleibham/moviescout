66f1492d623440aaa4ba2114e1e8d4cc7b350cd8
Felix Kirchhoff <mailfk@phispace.eu>
2021-11-15 15:53:30 +0100

    Refactor use Object to map different component options

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -2,50 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 type TypographyProps = {
-  type: string;
+  type: 'h1' | 'h2' | 'h3' | 'h4';
   children: React.ReactNode;
 };
 
 function Typography({ type, children }: TypographyProps): JSX.Element {
-  const HeadingElement = getElementByType(type);
+  const ElementMap = {
+    h1: H1,
+    h2: H2,
+    h3: H3,
+    h4: H4,
+  };
 
-  return <HeadingElement>{children}</HeadingElement>;
-}
+  const Heading = ElementMap[type];
 
-function getElementByType(type: string) {
-  switch (type.toUpperCase()) {
-    case 'H1':
-      return H1;
-    case 'H2':
-      return H2;
-    case 'H3':
-      return H3;
-    case 'H4':
-      return H4;
-    default:
-      return H1;
-  }
+  return <Heading>{children}</Heading>;
 }
 
 export default Typography;
 
 const H1 = styled.h1`
-  font-size: 30px;
-  line-height: 45px;
+  font-size: 1.875rem;
   font-weight: 700;
 `;
 const H2 = styled.h2`
-  font-size: 20px;
-  line-height: 30px;
+  font-size: 1.25rem;
   font-weight: 700;
 `;
 const H3 = styled.h3`
-  font-size: 18px;
+  font-size: 1.125rem;
   line-height: 27px;
   font-weight: 700;
 `;
 const H4 = styled.h3`
-  font-size: 18px;
-  line-height: 27px;
+  font-size: 1.125rem;
   font-weight: 400;
 `;
