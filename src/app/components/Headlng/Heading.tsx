2273034ae0f15794f6fcfe51a722ec5d61066944
Felix Kirchhoff <mailfk@phispace.eu>
2021-11-15 14:48:25 +0100

    Set headline1 as a h1 element, set healine2 as a h2 element

--- src/app/components/Headlng/Heading.tsx
+++ src/app/components/Headlng/Heading.tsx
@@ -7,17 +7,17 @@ type HeadingProps = {
 };
 
 function Heading({ size, children }: HeadingProps): JSX.Element {
-  return size === 'large' ? <H2>{children}</H2> : <H3>{children}</H3>;
+  return size === 'large' ? <H1>{children}</H1> : <H2>{children}</H2>;
 }
 
 export default Heading;
 
-const H2 = styled.h2`
+const H1 = styled.h1`
   font-size: 30px;
   line-height: 45px;
   font-weight: 700;
 `;
-const H3 = styled.h3`
+const H2 = styled.h2`
   font-size: 20px;
   line-height: 30px;
   font-weight: 700;
