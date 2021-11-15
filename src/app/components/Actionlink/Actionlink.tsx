d62758c8ffd90dbce33751787e89b04668cc064c
Julian Buchholz <github@julianbuchholz.com>
2021-11-15 18:49:49 +0100

    Fix styling for button

--- src/app/components/Actionlink/Actionlink.tsx
+++ src/app/components/Actionlink/Actionlink.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import ActionLinkIcon from '../Icons/ActionlinkIcon';
 
 type ActionlinkProps = {
   url: string;
@@ -16,35 +17,10 @@ function ActionLink({ url, text }: ActionlinkProps): JSX.Element {
       onMouseEnter={() => setHidden(false)}
       onMouseLeave={() => setHidden(true)}
     >
-      {text}
+      <LinkText>{text}</LinkText>
       {!hidden && (
         <Icon>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-          >
-            <rect width="24" height="24" fill="url(#pattern0)" />
-            <defs>
-              <pattern
-                id="pattern0"
-                patternContentUnits="objectBoundingBox"
-                width="1"
-                height="1"
-              >
-                <use xlinkHref="#image0_219_294" transform="scale(0.0333333)" />
-              </pattern>
-              <image
-                id="image0_219_294"
-                width="30"
-                height="30"
-                xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAcklEQVRIie3WMQ6CUBAG4S/cQYL3v4mNCVJh4XGgwIrIC+SpifGfdorZbLFZwj9zwYD2oKumx4T7i0DJVXPC+Aw8cN7pEk888d+Jt5YDMuF2wIHm3dN8mvU6u50u0UQT/V6U5eJsvTclV83V9kNXciGAGVM9cX2eizuGAAAAAElFTkSuQmCC"
-              />
-            </defs>
-          </svg>
+          <ActionLinkIcon />
         </Icon>
       )}
     </Actionlink>
@@ -58,15 +34,23 @@ const Actionlink = styled.a`
   color: #111111;
   text-transform: uppercase;
   text-decoration: none;
-  padding: 13px 105px;
   border-radius: 5px;
   font-size: 1rem;
   font-weight: 500;
-  position: relative;
+  margin-left: 20px;
+  margin-right: 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  justify-items: center;
+  min-height: 30px;
+`;
+
+const LinkText = styled.span`
+  grid-column: 2/5;
+  align-self: center;
 `;
 
 const Icon = styled.span`
-  position: absolute;
-  right: 10px;
-  top: 22%;
+  grid-column: 5/6;
+  align-self: center;
 `;
