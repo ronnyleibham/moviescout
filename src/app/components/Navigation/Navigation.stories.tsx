27914ac39b5f6a7679220904f0d9dd81ae76fb9d
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-15 12:26:05 +0100

    Add storybook

---
+++ src/app/components/Navigation/Navigation.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+export default {
+  title: 'Component/Navigation',
+  component: Navigation,
+};
+
+export const Nav = (): JSX.Element => <Navigation />;
