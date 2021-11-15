e9fbe1db47f049662ef4265f38c417bfa25f97c2
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-15 12:24:36 +0100

    Add basic styling to ul

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import styled from 'styled-components'
 
 export default function Navigation(): JSX.Element {
   return (
-      <ul>
+      <List>
         <li>Home</li>
         <li>Search</li>
         <li>Bookmarks</li>
-      </ul>
+      </List>
   );
 }
+
+const List = styled.ul`
+    background-color: black;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`
