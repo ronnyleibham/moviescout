785f0c94e93bf077384cd998bb282858541ed5ab
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-16 11:03:45 +0100

    Add Icons and logic to Nav bar

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,20 +1,75 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import BookmarkIcon from '../Icons/BookmarkIcon';
+import SearchIcon from '../Icons/SearchIcon';
+import HomeIcon from '../Icons/HomeIcon';
+
+const [isHomeActive, setIsHomeActive] = useState(true);
+const [isSearchActive, setIsSearchActive] = useState(false);
+const [isBookmarksActive, setIsBookmarksActive] = useState(false);
 
 export default function Navigation(): JSX.Element {
   return (
-      <List>
-        <li>Home</li>
-        <li>Search</li>
-        <li>Bookmarks</li>
-      </List>
+    <List>
+      <li>
+        <StyledLink
+          to="/"
+          onClick={() => {
+            setIsHomeActive(true);
+            setIsBookmarksActive(false);
+            setIsSearchActive(false);
+          }}
+          status={isHomeActive}
+        >
+          <HomeIcon fill={isHomeActive ? 'yellow' : 'none'} />
+        </StyledLink>
+      </li>
+      <li>
+        <StyledLink
+          to="/search"
+          onClick={() => {
+            setIsHomeActive(false);
+            setIsBookmarksActive(false);
+            setIsSearchActive(true);
+          }}
+          status={isSearchActive}
+        >
+          <SearchIcon fill={isHomeActive ? 'yellow' : 'none'} />
+        </StyledLink>
+      </li>
+      <li>
+        <StyledLink
+          to="/bookmarks"
+          onClick={() => {
+            setIsHomeActive(false);
+            setIsBookmarksActive(true);
+            setIsSearchActive(false);
+          }}
+          status={isBookmarksActive}
+        >
+          <BookmarkIcon fill={isHomeActive ? 'yellow' : 'none'} />
+        </StyledLink>
+      </li>
+    </List>
   );
 }
 
 const List = styled.ul`
-    background-color: black;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-`
+  background-color: #0b0b0b;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  padding: 1rem 1.5rem;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+`;
+
+type LinkProps = {
+  status: boolean;
+};
+
+const StyledLink = styled(Link)<LinkProps>`
+  text-decoration: none;
+`;
