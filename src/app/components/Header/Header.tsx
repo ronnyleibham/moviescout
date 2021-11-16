0d76b32f4767818d62c698b909663da2b3b983d0
PReichetanz <paul.reichetanzgmx.de>
2021-11-16 11:11:48 +0100

    Refactor: add colors to arrow

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -25,20 +25,23 @@ export default function Header({ children, style }: HeaderProps): JSX.Element {
       {headerStyle === 'withBackButton' && (
         <Container>
           <H1>
-            <BackButton>
+            <BackButton__yellow>
               <svg
-                width="15"
-                height="24"
-                viewBox="0 0 15 24"
-                fill="currentColor"
                 xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
                 <path
-                  d="M5.50305 11.9496L13.567 3.88571C13.9912 3.45891 14.2294 2.88156 14.2294 2.27976C14.2294 1.67796 13.9912 1.10061 13.567 0.673812C13.3552 0.460304 13.1033 0.290837 12.8257 0.175189C12.5481 0.0595409 12.2504 0 11.9496 0C11.6489 0 11.3512 0.0595409 11.0736 0.175189C10.796 0.290837 10.5441 0.460304 10.3323 0.673812L0.673812 10.3323C0.460304 10.5441 0.290838 10.796 0.17519 11.0736C0.059542 11.3512 0 11.6489 0 11.9496C0 12.2504 0.059542 12.5481 0.17519 12.8257C0.290838 13.1033 0.460304 13.3552 0.673812 13.567L10.3323 23.3394C10.5451 23.5505 10.7976 23.7175 11.0751 23.8309C11.3527 23.9442 11.6498 24.0017 11.9496 24C12.2494 24.0017 12.5466 23.9442 12.8242 23.8309C13.1017 23.7175 13.3541 23.5505 13.567 23.3394C13.9912 22.9126 14.2294 22.3352 14.2294 21.7334C14.2294 21.1316 13.9912 20.5543 13.567 20.1275L5.50305 11.9496Z"
-                  fill="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 19l-7-7 7-7"
                 />
               </svg>
-            </BackButton>
+            </BackButton__yellow>
             {children}
             <Span>.</Span>
           </H1>
@@ -46,20 +49,23 @@ export default function Header({ children, style }: HeaderProps): JSX.Element {
       )}
       {headerStyle === 'actionOnly' && (
         <Container>
-          <BackButton>
-            <Svg
-              width="15"
-              height="24"
-              viewBox="0 0 15 24"
-              fill="currentColor"
+          <BackButton__white>
+            <svg
               xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
               <path
-                d="M5.50305 11.9496L13.567 3.88571C13.9912 3.45891 14.2294 2.88156 14.2294 2.27976C14.2294 1.67796 13.9912 1.10061 13.567 0.673812C13.3552 0.460304 13.1033 0.290837 12.8257 0.175189C12.5481 0.0595409 12.2504 0 11.9496 0C11.6489 0 11.3512 0.0595409 11.0736 0.175189C10.796 0.290837 10.5441 0.460304 10.3323 0.673812L0.673812 10.3323C0.460304 10.5441 0.290838 10.796 0.17519 11.0736C0.059542 11.3512 0 11.6489 0 11.9496C0 12.2504 0.059542 12.5481 0.17519 12.8257C0.290838 13.1033 0.460304 13.3552 0.673812 13.567L10.3323 23.3394C10.5451 23.5505 10.7976 23.7175 11.0751 23.8309C11.3527 23.9442 11.6498 24.0017 11.9496 24C12.2494 24.0017 12.5466 23.9442 12.8242 23.8309C13.1017 23.7175 13.3541 23.5505 13.567 23.3394C13.9912 22.9126 14.2294 22.3352 14.2294 21.7334C14.2294 21.1316 13.9912 20.5543 13.567 20.1275L5.50305 11.9496Z"
-                fill="white"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15 19l-7-7 7-7"
               />
-            </Svg>
-          </BackButton>
+            </svg>
+          </BackButton__white>
           <Bookmark>
             <svg
               width="17"
@@ -81,6 +87,33 @@ export default function Header({ children, style }: HeaderProps): JSX.Element {
   );
 }
 
+const BackButton__white = styled.button`
+  background-color: #111;
+  color: #fff;
+  padding-right: 25px;
+  border: none;
+`;
+
+const BackButton__yellow = styled.button`
+  background-color: #111;
+  color: #ffc700;
+  padding-right: 25px;
+  border: none;
+`;
+
+const Bookmark = styled.button`
+  background-color: #111;
+  border: none;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: #111;
+  padding-left: 15px;
+  padding-right: 30px;
+`;
+
 const H1 = styled.h1`
   color: #fff;
   font-family: Poppins;
@@ -91,25 +124,4 @@ const H1 = styled.h1`
 
 const Span = styled.span`
   color: #ffc700;
-`;
-
-const BackButton = styled.button`
-  background-color: #111;
-  padding-right: 25px;
-`;
-
-const Bookmark = styled.button`
-  background-color: #111;
-`;
-
-const Svg = styled.svg`
-  color: #ffc700;
-`;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background-color: #111;
-  padding-left: 15px;
-  padding-right: 30px;
 `;
