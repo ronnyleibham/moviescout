5b4174d9d17e7c22c7428a4ccc58d0466d19f92c
PReichetanz <paul.reichetanzgmx.de>
2021-11-16 12:51:17 +0100

    Add BackIcon.tsx

---
+++ src/app/components/Icons/BackIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function BookmarkIcon(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15 19l-7-7 7-7"
+      />
+    </svg>
+  );
+}
