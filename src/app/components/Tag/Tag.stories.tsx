e0e9545b3a79191c9bbfdfe9560a223cd9366e7c
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-16 11:35:45 +0100

    update: add different tag versions to storybook

--- src/app/components/Tag/Tag.stories.tsx
+++ src/app/components/Tag/Tag.stories.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Tag from './Tag';
+
 export default {
   title: 'Component/Tag',
   component: Tag,
 };
+
 export const NotActive = (): JSX.Element => (
-  <Tag tagStatus={false}> Action</Tag>
+  <Tag numberOfItems={3} tagStatus={false}>
+    Action
+  </Tag>
 );
-export const Active = (): JSX.Element => <Tag tagStatus={true}>Action</Tag>;
+export const Active = (): JSX.Element => (
+  <Tag numberOfItems={3} tagStatus={true}>
+    Action
+  </Tag>
+);
+export const All = (): JSX.Element => <Tag tagStatus={true}>Action</Tag>;
