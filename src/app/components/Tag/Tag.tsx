8993cbc6c5e4cc944f991e033fc7aba6d1c8fecb
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-16 11:34:01 +0100

    feature: incorporate numberOfItems prop

--- src/app/components/Tag/Tag.tsx
+++ src/app/components/Tag/Tag.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import type { ReactNode } from 'react';
 import styled from 'styled-components';
+
 type TagProps = {
   children: ReactNode;
   tagStatus?: boolean;
+  numberOfItems?: number;
 };
-export default function Tag({ children, tagStatus }: TagProps): JSX.Element {
-  return <TagStyleActive tagStatus={tagStatus}>{children}</TagStyleActive>;
+
+export default function Tag({
+  children,
+  tagStatus,
+  numberOfItems,
+}: TagProps): JSX.Element {
+  return (
+    <TagStyleActive tagStatus={tagStatus}>{`${children} ${
+      numberOfItems ? numberOfItems : ''
+    }`}</TagStyleActive>
+  );
 }
 const TagStyleActive = styled.span<Partial<TagProps>>`
   background-color: ${(props) => (props.tagStatus ? '#FFC700' : '#2A2A2A')};
