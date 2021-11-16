a7839216462c77348b740b60700731eac726b376
Patrick Möller <patrick_moeller@live.de>
2021-11-16 10:35:33 +0100

    Adding new SVG

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ButtonBookmark from './components/ButtonBookmark/ButtonBookmark';
+import ButtonHome from './components/ButtonHome/ButtonHome';
 
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(0);
-  return <ButtonBookmark clicked={false} />;
+  return <ButtonHome clicked={true} />;
 }
 
 export default App;
