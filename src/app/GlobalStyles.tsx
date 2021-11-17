227868c525c56658fe436fff33790b4931d5fee7
Ronny Leibham <ronnyleibham@gmx.de>
2021-11-17 15:56:40 +0100

    Create GlobalStyles with styled-components

---
+++ src/app/GlobalStyles.tsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    *,*::before,*::after {
+        box-sizing: border-box;
+    }
+
+    body {
+    margin: 0;
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+        'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+        'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: white;
+    background-color: black;
+    }
+
+    code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }
+`;
+
+export default GlobalStyles;
